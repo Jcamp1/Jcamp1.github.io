1763076abaf9b66f06d804c7b51a1f53986bc49f
Jcamp1 <34587411+Jcamp1@users.noreply.github.com>
2022-02-21 19:26:27 -0500

    Update bus.js

--- bus.js
+++ bus.js
@@ -34,7 +34,7 @@ let busMarkers = {
 };
 
 //create the map objects tile layer
-L.tileLayer("https://api.mapbox.com/styles/v1/examples/cjikt35x83t1z2rnxpdmjs7y7?access_token=pk.eyJ1IjoiamNhbXAxIiwiYSI6ImNrenhkbnMyYjAwZ3QydXF2eW4xanNpYjMifQ.uTKFUE_SYaEd-PAPMU4w_w", {
+L.tileLayer("https://api.mapbox.com/styles/v1/examples/cjikt35x83t1z2rnxpdmjs7y7?access_token={accessToken}", {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     id: 'mapbox.streets',
